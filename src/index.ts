31ce99a15b483db3fbb37be97c37812bed8fa427
Ron B <me@ronthecookie.me>
2020-09-10 21:39:37 +0300

    add extra safety & automatically get cookiecord version based on this package's

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,31 @@
+import { spawnSync } from "child_process";
 import program from "commander";
-import { copy, readJSON, writeFile } from "fs-extra";
+import { copy, pathExists, readdir, readJSON, writeFile } from "fs-extra";
 import { join, resolve } from "path";
-import { spawn, spawnSync } from "child_process";
 program.version("0.0.1").name("cookiecord-generator");
 
 program
 	.command("generate <name>")
 	.description("generate a new cookiecord bot")
-	.action(async name => {
+	.action(async (name: string) => {
 		const genPath = resolve(__dirname, "../../gen");
 		const selfPkgJson = await readJSON(
-			resolve(__dirname, "../package.json")
+			resolve(__dirname, "../../package.json")
 		);
+		const targetPath = join(process.cwd(), name);
+		try {
+			if (
+				(await pathExists(targetPath)) &&
+				(await readdir(targetPath)).length !== 0
+			)
+				throw new Error("Target directory exists and is not empty");
+		} catch (error) {
+			console.error(error.message);
+			process.exit(1);
+		}
 
-		await copy(genPath, join(process.cwd(), name));
+		await copy(genPath, targetPath);
+
 		const pkgjson = await readJSON(join(genPath, "package.json"));
 		pkgjson.name = name;
 		pkgjson.dependencies.cookiecord = `^${
@@ -23,7 +35,7 @@ program
 			join(process.cwd(), name, "package.json"),
 			JSON.stringify(pkgjson, null, 4)
 		);
-		spawnSync("yarn", { cwd: join(process.cwd(), name), stdio: "inherit" });
+		spawnSync("yarn", { cwd: targetPath, stdio: "inherit" });
 		console.log(
 			`Generated at ${join(
 				process.cwd(),
