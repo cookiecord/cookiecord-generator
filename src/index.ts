1ac8d4a6bef753f12faad4b323f0421cb55a680c
Ron B <me@ronthecookie.me>
2020-09-10 21:20:07 +0300

    automatically set cookiecord version based on this package's version

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,12 @@ program
     .description("generate a new cookiecord bot")
     .action(async name => {
         const genPath = resolve(__dirname, "../../gen");
+		const selfPkgJson = await readJSON(resolve(__dirname, "../package.json"));
 
         await copy(genPath, join(process.cwd(), name));
         const pkgjson = await readJSON(join(genPath, "package.json"));
-        pkgjson.name = name;
+		pkgjson.name = name;
+		pkgjson.dependencies.cookiecord = `^${selfPkgJson.version.split("-")[0]}`;
         await writeFile(
             join(process.cwd(), name, "package.json"),
             JSON.stringify(pkgjson, null, 4)
