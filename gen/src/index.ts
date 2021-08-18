8e061fc454f4d7a59cb2a394c509a22d05c0699f
ckie <git-525ff67@ckie.dev>
2021-08-18 08:40:09 +0300

    v0.7.18

--- gen/src/index.ts
+++ gen/src/index.ts
@@ -1,10 +1,16 @@
 import CookiecordClient from "cookiecord";
 import dotenv from "dotenv-safe";
+import { Intents } from "discord.js";
 dotenv.config();
 
-const client = new CookiecordClient({
-    botAdmins: process.env.BOT_ADMINS?.split(",")
-});
+const client = new CookiecordClient(
+  {
+    botAdmins: process.env.BOT_ADMINS?.split(","),
+  },
+  {
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  }
+);
 
 client.loadModulesFromFolder("src/modules");
 client.reloadModulesFromFolder("src/modules");
